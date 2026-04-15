552655fbde2017886a8ed630b9d45d30efe42711
Darin Butler <butlerdarin@gmail.com>
2026-04-15 16:22:31 +0100

    Add hero background image to cruise-lines listing page

---
+++ _next/static/lwcqFgP6852Nx-bpcO3wz/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog\u002F[slug]","\u002Fcruise-lines\u002F[slug]","\u002Fdestinations\u002F[slug]","\u002Fnz-cruise-insurance\u002F[term]","\u002Fpassengers\u002F[slug]","\u002Fregions\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
